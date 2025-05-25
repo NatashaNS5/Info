ceccab9fdb3c8537181910bb6f973c4ca959620f
Natasha <nataha1654@gmail.com>
2025-05-25 17:00:47 +0300

    Добавьте файлы проекта.

---
+++ Info/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Info.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Info.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Auditorium> Auditoriums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = @"C:\Users\USER\source\repos\Info\Info\auditoriums.db"; 
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+    }
+}
---
+++ Info/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Info.Data;
+
+namespace Info
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadAuditoriums();
+        }
+
+        private void LoadAuditoriums()
+        {
+            using (var context = new AppDbContext())
+            {
+                List<string> auditoriums = context.Auditoriums
+                    .Where(a => a.Floor == 5)
+                    .OrderBy(a => a.Name)
+                    .Select(a => a.Name)
+                    .ToList();
+                AuditoriumsListBox.ItemsSource = auditoriums;
+            }
+        }
+    }
+}
